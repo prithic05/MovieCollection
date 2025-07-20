c17bfcf392bcb8322e1fe778be2a9c6e6888167e
prithic05 <prithickmonish13@gmail.com>
2025-07-20 15:41:16 +0530

    code for Homepage

---
+++ Homepage/script.js
@@ -0,0 +1,12 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.3 });
+
+faders.forEach(fader => appearOnScroll.observe(fader));
